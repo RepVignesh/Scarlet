8b490c651e7494e2b7af384889de2e4d13b4928f
Venkatesh K <venkateshdkm1629@gmail.com>
2026-08-04 14:34:04 +0530

    Delete utils

--- backend/src/utils/domain.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import urlparse
-
-def extract_domain(domain: str) -> str:
-    if "://" in domain:
-        domain = urlparse(domain).hostname or domain
-
-    return domain.lower()
