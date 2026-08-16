9b47c9ade30b30721740bc889ab116ae9a703289
Shree Varshan <shreevarshan822@gmail.com>
2026-08-16 20:24:14 +0530

    Add URLInputBar to MainPage (PlaceHolder)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import InputBar from "./components/Inputs/InputBar";
+
 export default function Page() {
-  return <></>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        paddingTop: "4rem",
+        height: "100%",
+      }}
+    >
+      <InputBar
+        placeholder="Enter an URL"
+        onSubmit={() => alert("URL Has been submitted")}
+      />
+    </div>
+  );
 }
