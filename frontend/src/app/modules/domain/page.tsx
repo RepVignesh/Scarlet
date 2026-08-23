a79972217d57c429a431f98195b3547f9bee98da
Shree Varshan <shreevarshan822@gmail.com>
2026-08-23 20:56:48 +0530

    Add Routing (Base Route Functionality)

--- frontend/src/app/modules/domain/page.tsx
+++ frontend/src/app/modules/domain/page.tsx
@@ -1,3 +1,47 @@
-export default function Domain() {
-  return <h1>Domain</h1>;
+"use client";
+
+import InputBar from "@/app/components/Inputs/InputBar";
+import { useState } from "react";
+import { Domain } from "@/app/interfaces/domain";
+import DomainInfoCard from "@/app/components/Cards/DomainInfo/DomainInfoCard";
+
+export default function Headers() {
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState<Domain>();
+
+  const getInformation = async (url: string) => {
+    setLoading(true);
+
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_ENDPOINT}/domain/?url=${encodeURIComponent(url)}`,
+      );
+
+      const data = await res.json();
+
+      setResponse(data);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          paddingTop: "4rem",
+          height: "100%",
+        }}
+      >
+        <InputBar placeholder="Enter an URL" onSubmit={getInformation} />
+      </div>
+
+      {loading && <p>Loading...</p>}
+
+      {response && <DomainInfoCard data={response} />}
+    </div>
+  );
 }
--- frontend/src/app/modules/headers/page.tsx
+++ frontend/src/app/modules/headers/page.tsx
@@ -1,3 +1,47 @@
+"use client";
+
+import InputBar from "@/app/components/Inputs/InputBar";
+import { useState } from "react";
+import { HTTPHeader } from "@/app/interfaces/domain";
+import HTTPHeadersCard from "@/app/components/Cards/HTTPHeaders/HTTPHeadersCard";
+
 export default function Headers() {
-  return <h1>Headers</h1>;
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState<HTTPHeader>();
+
+  const getInformation = async (url: string) => {
+    setLoading(true);
+
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_ENDPOINT}/domain/headers/?url=${encodeURIComponent(url)}`,
+      );
+
+      const data = await res.json();
+
+      setResponse(data);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          paddingTop: "4rem",
+          height: "100%",
+        }}
+      >
+        <InputBar placeholder="Enter an URL" onSubmit={getInformation} />
+      </div>
+
+      {loading && <p>Loading...</p>}
+
+      {response && <HTTPHeadersCard data={response} />}
+    </div>
+  );
 }
